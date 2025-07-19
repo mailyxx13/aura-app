37b3f1af8a164df2398ec8bf90f2175973a5794f
mailyxx13 <mailyxx13@gmail.com>
2025-07-20 00:46:50 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/**/*.{js,jsx}',
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+
